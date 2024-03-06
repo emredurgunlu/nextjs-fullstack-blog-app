51125d2c7086eb91c55a041e78c6d746df8ae39b
emre durgunlu <emredurgunlu@gmail.com>
2024-03-07 01:32:24 +0300

    some styling

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,15 +1,16 @@
 const { default: Link } = require("next/link");
+import styles from "./links.module.css";
 
 const Links = () => {
   const links = [
     { title: "Homepage", path: "/" },
     { title: "About", path: "/about" },
     { title: "Contact", path: "/contact" },
-    { title: "Blog", path: "/blog"}
+    { title: "Blog", path: "/blog" },
   ];
 
   return (
-    <div>
+    <div className={styles.links}>
       {links.map((link) => (
         <Link href={link.path} key={link.title}>
           {link.title}
