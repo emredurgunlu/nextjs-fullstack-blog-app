15596f097b729c45e997090b40e4e56d72edcc8e
emre durgunlu <emredurgunlu@gmail.com>
2024-03-07 17:26:35 +0300

    styling for blog detail page

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,45 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
 const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/20433278/pexels-photo-20433278/free-photo-of-indian-blue-jay.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            src="https://images.pexels.com/photos/20433278/pexels-photo-20433278/free-photo-of-indian-blue-jay.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={50}
+            height={50}
+            className={styles.avatar}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Emre Durgunlu</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, beatae.
+          Explicabo eligendi possimus inventore cumque quisquam aliquid, quod
+          sequi doloribus sit a ullam, iusto provident expedita quidem placeat
+          blanditiis hic.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
