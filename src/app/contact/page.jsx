7020d3ac730e1700979bcaea9669282b46b6da90
emre durgunlu <emredurgunlu@gmail.com>
2024-03-07 16:33:38 +0300

    styling for contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,31 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      {/* home ve about sayfalarında .imgContainer sınıfıma yükseklik vermedik çünkü yazıların sağında kaldığı için yazıların yüksekliğini aldı faka
+      contact sayfasında yazıların solunda kaldığı için referans alacağı yükseklik olmadığı için yükseklik vermezsek hata verir */}
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
